0cba13cdf679e2a01cc874f3d8e1b33c477ec858
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-23 15:32:50 -0300

    caso de uso para criar novo usuário

---
+++ src/use-case/user/create-user-use-case.ts
@@ -0,0 +1,34 @@
+import { UserInterface } from '../../domain/user/interface/user-interface';
+import { User } from '../../domain/user/user';
+import { UserRepositoryInterface } from '../../infra/repositories/user-repository-interface';
+import { error, success } from '../../shared/response';
+
+export class CreateUserUseCase {
+  private userRepository: UserRepositoryInterface;
+
+  constructor(userRepository: UserRepositoryInterface) {
+    this.userRepository = userRepository;
+  }
+
+  public async perform(user: UserInterface) {
+    const userOrError = User.create(user.username, user.email, user.password);
+
+    if (userOrError.isError()) return error(userOrError.value);
+
+    const findUserByUsername = await this.userRepository.findByUsername(
+      user.username,
+    );
+
+    if (findUserByUsername.id) return error('This username already exists');
+
+    const findUserByEmail = await this.userRepository.findByEmail(user.email);
+
+    if (findUserByEmail.id) return error('This email already exists');
+
+    const createUserResponse = await this.userRepository.create(user);
+
+    return success({
+      id: createUserResponse.id,
+    });
+  }
+}
