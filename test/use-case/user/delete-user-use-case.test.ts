0b701dfb6291eab2394a290d30a6b514b6d2832f
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-26 11:12:25 -0300

    testes no caso de uso para deletar user pelo id

---
+++ test/use-case/user/delete-user-use-case.test.ts
@@ -0,0 +1,32 @@
+import { CreateUserUseCase } from '../../../src/use-case/user/create-user-use-case';
+import { DeleteUserUseCase } from '../../../src/use-case/user/delete-user-use-case';
+import { InMemoryUserRepository } from '../../util/repositories/in-memory-user-repository';
+
+describe('Delete user use case tests', () => {
+  const userRepository = new InMemoryUserRepository();
+  const deleteUserUseCase = new DeleteUserUseCase(userRepository);
+  const createUserUseCase = new CreateUserUseCase(userRepository);
+
+  it('Should delete user', async () => {
+    const userData = {
+      username: 'a',
+      email: 'a@gmail.com',
+      password: '123456',
+    };
+
+    const createUserResponse = await createUserUseCase.perform(userData);
+
+    const userId = createUserResponse.value.id;
+
+    const deleteUserResponse = await deleteUserUseCase.perform(userId);
+
+    expect(deleteUserResponse.isSuccess()).toEqual(true);
+  });
+
+  it('Should delete user', async () => {
+    const deleteUserResponse = await deleteUserUseCase.perform('0');
+
+    expect(deleteUserResponse.isError()).toEqual(true);
+    expect(deleteUserResponse.value).toEqual('User not found');
+  });
+});
