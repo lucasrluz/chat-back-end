40a006b713c5fa0daaa887ee5ce3d696dc38c299
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-26 10:06:25 -0300

    novos métodos no repositório para modificação do user

--- src/infra/repositories/user-repository-interface.ts
+++ src/infra/repositories/user-repository-interface.ts
@@ -2,6 +2,14 @@ import { UserInterface } from '../../domain/user/interface/user-interface';
 
 export interface UserRepositoryInterface {
   create(user: UserInterface): Promise<{ id: string }>;
+  findById(id: string): Promise<{
+    id: string | undefined;
+    username: string | undefined;
+    email: string | undefined;
+    password: string | undefined;
+  }>;
   findByUsername(username: string): Promise<{ id: string | undefined }>;
   findByEmail(email: string): Promise<{ id: string | undefined }>;
+  updateUsername(userId: string, username: string): Promise<void>;
+  updatePassword(userId: string, password: string): Promise<void>;
 }
