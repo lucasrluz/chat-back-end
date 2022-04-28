193f5ec7b0e121ff604e7507f674c3230c6519f3
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-28 10:54:52 -0300

    função que criar nova instância do controlador para criar usuário

---
+++ src/infra/controllers/factories/make-create-user-controller.ts
@@ -0,0 +1,11 @@
+import { CreateUserUseCase } from '../../../use-case/user/create-user-use-case';
+import { PrismaUserRepository } from '../../external/prisma/repositories/prisma-user-repository';
+import { CreateUserController } from '../create-user-controller';
+
+export function makeCreateUserController() {
+  const userRepository = new PrismaUserRepository();
+  const createUserUseCase = new CreateUserUseCase(userRepository);
+  const createUserController = new CreateUserController(createUserUseCase);
+
+  return createUserController;
+}
