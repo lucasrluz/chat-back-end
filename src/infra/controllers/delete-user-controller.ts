8ee244ee7847d8773e40249f09094abe72789e48
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-29 13:41:02 -0300

    controlador para deletar usuário

---
+++ src/infra/controllers/delete-user-controller.ts
@@ -0,0 +1,18 @@
+import { DeleteUserUseCase } from '../../use-case/user/delete-user-use-case';
+import { notFound, ok } from './respose/http-responses';
+
+export class DeleteUserController {
+  private deleteUserUseCase: DeleteUserUseCase;
+
+  constructor(deleteUserUseCase: DeleteUserUseCase) {
+    this.deleteUserUseCase = deleteUserUseCase;
+  }
+
+  public async perform(userId: string) {
+    const response = await this.deleteUserUseCase.perform(userId);
+
+    if (response.isError()) return notFound(response.value);
+
+    return ok(response.value);
+  }
+}
