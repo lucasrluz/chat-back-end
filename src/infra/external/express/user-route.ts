0930f35749a06f69261ba1d8d109df5dc953c936
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-29 13:42:34 -0300

    rota para deletar usuário

--- src/infra/external/express/user-route.ts
+++ src/infra/external/express/user-route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { createUserAdaptRoute } from './adapters/create-user-adapt-route';
+import { deleteUserAdaptRoute } from './adapters/delete-user-adapt-route';
 import { editUserAdaptRoute } from './adapters/edit-user-adapt-route';
 
 export const userRouter = Router();
 
 userRouter.post('/user', createUserAdaptRoute());
 userRouter.put('/user/:user_id', editUserAdaptRoute());
+userRouter.delete('/user/:user_id', deleteUserAdaptRoute());
