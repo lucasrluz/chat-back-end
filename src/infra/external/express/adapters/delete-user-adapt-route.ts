9a6354af4782272cb497a5501c6a15a15ab491ce
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-29 13:42:10 -0300

    função para adaptação da rota para deletar usuário

---
+++ src/infra/external/express/adapters/delete-user-adapt-route.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { makeDeleteUserController } from '../../../controllers/factories/make-delete-user-controller';
+
+export function deleteUserAdaptRoute() {
+  return async (req: Request, res: Response) => {
+    const userId = req.params.user_id;
+
+    const deleteUserController = await makeDeleteUserController();
+
+    const response = await deleteUserController.perform(userId);
+
+    return res.status(response.status).json(response.value);
+  };
+}
