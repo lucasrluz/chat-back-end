8a866d0dae558ac63db31c407d53650b5c614bc4
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-29 10:22:04 -0300

    rota para editar usuário

--- src/infra/external/express/user-route.ts
+++ src/infra/external/express/user-route.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { createUserAdaptRoute } from './adapters/create-user-adapt-route';
+import { editUserAdaptRoute } from './adapters/edit-user-adapt-route';
 
 export const userRouter = Router();
 
 userRouter.post('/user', createUserAdaptRoute());
+userRouter.put('/user/:user_id', editUserAdaptRoute());
