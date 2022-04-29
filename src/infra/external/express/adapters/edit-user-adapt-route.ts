413d9fcd36b0221c03780828a3dc933ce8434c90
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-29 10:21:43 -0300

    função para adaptação da rota para editar usuário

---
+++ src/infra/external/express/adapters/edit-user-adapt-route.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { makeEditUserController } from '../../../controllers/factories/make-edit-user-controller';
+
+export function editUserAdaptRoute() {
+  return async (req: Request, res: Response) => {
+    const editUserData = {
+      userId: req.params.user_id,
+      username: req.body.username,
+      password: req.body.password,
+    };
+
+    const editUserController = makeEditUserController();
+
+    const response = await editUserController.perform(editUserData);
+
+    return res.status(response.status).json(response.value);
+  };
+}
