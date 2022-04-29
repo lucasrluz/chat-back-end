d7b2d93a48767c45a79e9c2bd8c034d83e00223b
lucasrluz <lucasr.luzbr@gmail.com>
2022-04-29 13:58:18 -0300

    adicionado dois testes na rota para criar usuário

--- test/routes/user/create-user.test.ts
+++ test/routes/user/create-user.test.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma, PrismaClient } from '@prisma/client';
 import request from 'supertest';
 import { app } from '../../../src/infra/external/express/app';
 
@@ -24,6 +24,29 @@ describe('Tests on the create user route', () => {
   });
 
   describe('Username tests', () => {
+    it('Should return error message', async () => {
+      const userData = {
+        username: 'a',
+        email: 'a@gmail.com',
+        password: '123456',
+      };
+
+      await prismaClient.user.create({
+        data: {
+          username: userData.username,
+          email: userData.email,
+          password: userData.password,
+        },
+      });
+
+      const response = await request(app).post('/user').send(userData);
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual('This username already exists');
+
+      await prismaClient.user.deleteMany();
+    });
+
     it('Should return error message', async () => {
       const userData = {
         username: '',
@@ -117,6 +140,35 @@ describe('Tests on the create user route', () => {
   });
 
   describe('Email tests', () => {
+    it('Should return error message', async () => {
+      const userData1 = {
+        username: 'a',
+        email: 'a@gmail.com',
+        password: '123456',
+      };
+
+      const userData2 = {
+        username: 'b',
+        email: 'a@gmail.com',
+        password: '123456',
+      };
+
+      await prismaClient.user.create({
+        data: {
+          username: userData1.username,
+          email: userData1.email,
+          password: userData1.password,
+        },
+      });
+
+      const response = await request(app).post('/user').send(userData2);
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual('This email already exists');
+
+      await prismaClient.user.deleteMany();
+    });
+
     it('Should return error message', async () => {
       const userData = {
         username: 'a',
