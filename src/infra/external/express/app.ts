2ad93189c8db96b59cc944e10582d2c441df1c21
lucasrluz <lucasr.luzbr@gmail.com>
2022-05-03 15:53:18 -0300

    habilitando rota no projeto

--- src/infra/external/express/app.ts
+++ src/infra/external/express/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { authRouter } from './auth-route';
 import { userRouter } from './user-route';
 
 export const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(authRouter);
