c79417eb74db1dac0df4661e4c559854ad2dc834
lucasrluz <lucasr.luzbr@gmail.com>
2022-05-09 09:48:20 -0300

    rota para criação de refresh token

--- src/infra/external/express/auth-route.ts
+++ src/infra/external/express/auth-route.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { adaptRouteCreateNewAccessToken } from './adapters/auth/adapt-route-create-new-access-token';
 import { adaptRouteLogin } from './adapters/auth/adapt-route-login';
 
 export const authRouter = Router();
 
 authRouter.post('/login', adaptRouteLogin());
+authRouter.post('/token/:user_id', adaptRouteCreateNewAccessToken());
