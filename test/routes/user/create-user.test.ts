86a30a501ebee822ed1742f109c0f9cc27089715
lucasrluz <lucasr.luzbr@gmail.com>
2022-05-09 13:24:27 -0300

    removendo importação não utilizada

--- test/routes/user/create-user.test.ts
+++ test/routes/user/create-user.test.ts
@@ -1,4 +1,4 @@
-import { prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import request from 'supertest';
 import { app } from '../../../src/infra/external/express/app';
 
