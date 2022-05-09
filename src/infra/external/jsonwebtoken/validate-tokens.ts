5de21742eb26f57dafd6c3e5d770ffe0c987985d
lucasrluz <lucasr.luzbr@gmail.com>
2022-05-09 09:39:50 -0300

    funções para validação de JWT

---
+++ src/infra/external/jsonwebtoken/validate-tokens.ts
@@ -0,0 +1,22 @@
+import { verify } from 'jsonwebtoken';
+import { error, success } from '../../../shared/response';
+
+export function validateAccessToken(token: string) {
+  try {
+    return success(
+      verify(token, process.env.ACCESS_TOKEN_PRIVATE_KEY as string),
+    );
+  } catch (err) {
+    return error(err);
+  }
+}
+
+export function validateRefreshToken(token: string) {
+  try {
+    return success(
+      verify(token, process.env.REFRESH_TOKEN_PRIVATE_KEY as string),
+    );
+  } catch (err) {
+    return error(err);
+  }
+}
