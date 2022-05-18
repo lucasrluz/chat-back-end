f4225f791e00194ad53686318a0e3911673b39e8
lucasrluz <lucasr.luzbr@gmail.com>
2022-05-18 12:02:48 -0300

    adicionado middleware nas rotas

--- src/infra/external/express/routes/user-route.ts
+++ src/infra/external/express/routes/user-route.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { ensureAuthenticatedUserAdaptRoute } from './adapters/auth/adapt-route-ensure-authenticated-user';
 import { createUserAdaptRoute } from './adapters/user/create-user-adapt-route';
 import { deleteUserAdaptRoute } from './adapters/user/delete-user-adapt-route';
 import { editUserAdaptRoute } from './adapters/user/edit-user-adapt-route';
@@ -6,5 +7,13 @@ import { editUserAdaptRoute } from './adapters/user/edit-user-adapt-route';
 export const userRouter = Router();
 
 userRouter.post('/user', createUserAdaptRoute());
-userRouter.put('/user/:user_id', editUserAdaptRoute());
-userRouter.delete('/user/:user_id', deleteUserAdaptRoute());
+userRouter.put(
+  '/user/:user_id',
+  ensureAuthenticatedUserAdaptRoute(),
+  editUserAdaptRoute(),
+);
+userRouter.delete(
+  '/user/:user_id',
+  ensureAuthenticatedUserAdaptRoute(),
+  deleteUserAdaptRoute(),
+);
