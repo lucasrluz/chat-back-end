f110e88b68456c748a13012b02a9e4b852274fa5
lucasrluz <lucasr.luzbr@gmail.com>
2022-05-18 15:05:37 -0300

    refatoração no middleware para verificação do token com id do usuário

--- src/infra/middleware/ensure-authenticated-user.ts
+++ src/infra/middleware/ensure-authenticated-user.ts
@@ -5,9 +5,9 @@ import {
 } from '../controllers/respose/http-responses';
 import { validateAccessToken } from '../external/jsonwebtoken/validate-tokens';
 
-export function ensureAuthenticatedUser(accessToken: string) {
+export function ensureAuthenticatedUser(accessToken: string, userId: string) {
   if (!accessToken) return notFound('Token not found');
-  const validateTokenResponse = validateAccessToken(accessToken);
+  const validateTokenResponse = validateAccessToken(accessToken, userId);
 
   if (validateTokenResponse.isError())
     return badRequest(validateTokenResponse.value);
