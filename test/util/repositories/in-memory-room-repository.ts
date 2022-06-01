112cc17529d31f4a41cee84e56fa527e8f944c74
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-01 13:49:32 -0300

    repositório em memória para testes com sala

---
+++ test/util/repositories/in-memory-room-repository.ts
@@ -0,0 +1,18 @@
+import { RoomRepositoryInterface } from '../../../src/infra/repositories/room-repository-interface';
+
+interface RoomData {
+  roomId: string;
+  name: string;
+}
+
+export class InMemoryRoomRepository implements RoomRepositoryInterface {
+  private rooms: RoomData[] = [];
+
+  public async create(name: string) {
+    const roomId = this.rooms.length + 1;
+
+    this.rooms.push({ roomId: roomId.toString(), name: name });
+
+    return { name: name };
+  }
+}
