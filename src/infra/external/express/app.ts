955b8931e2373fa057977c5fcbf9c44781c937a1
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-01 13:48:37 -0300

    habilitando rotas da sala

--- src/infra/external/express/app.ts
+++ src/infra/external/express/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { authRouter } from './routes/auth-route';
+import { roomRouter } from './routes/room-route';
 import { userRouter } from './routes/user-route';
 
 export const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(roomRouter);
 app.use(authRouter);
