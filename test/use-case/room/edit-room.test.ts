895cb533139ef59ced25a4260de1e3b662af1cd1
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-02 10:47:53 -0300

    teste no caso de uso para editar sala

---
+++ test/use-case/room/edit-room.test.ts
@@ -0,0 +1,29 @@
+import { CreateRoomUseCase } from '../../../src/use-case/room/create-room-use-case';
+import { EditRoomUseCase } from '../../../src/use-case/room/edit-room-use-case';
+import { InMemoryRoomRepository } from '../../util/repositories/in-memory-room-repository';
+
+describe('Edit room use case tests', () => {
+  const roomRepository = new InMemoryRoomRepository();
+  const createRoomUseCase = new CreateRoomUseCase(roomRepository);
+  const editRoomUseCase = new EditRoomUseCase(roomRepository);
+
+  it('Should edit room', async () => {
+    const roomData = {
+      name: 'a',
+    };
+
+    const editRoomData = {
+      name: 'b',
+    };
+
+    const createRoomResponse = await createRoomUseCase.perform(roomData.name);
+
+    const editRoomResponse = await editRoomUseCase.perform(
+      createRoomResponse.value.roomId,
+      editRoomData.name,
+    );
+
+    expect(editRoomResponse.isSuccess()).toEqual(true);
+    expect(editRoomResponse.value).toEqual(editRoomData);
+  });
+});
