2fda72aeb5120ea31f9dc3898f31559e9399b7f3
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-02 09:06:18 -0300

    adicionado retorno do id ao criar nova sala

--- src/infra/external/prisma/repositories/prisma-room-repository.ts
+++ src/infra/external/prisma/repositories/prisma-room-repository.ts
@@ -2,10 +2,11 @@ import { RoomRepositoryInterface } from '../../../repositories/room-repository-i
 import { prismaClient } from '../prisma-client';
 
 export class PrismaRoomRepository implements RoomRepositoryInterface {
-  async create(name: string): Promise<{ name: string }> {
+  async create(name: string): Promise<{ roomId: string; name: string }> {
     const response = await prismaClient.room.create({ data: { name: name } });
 
     return {
+      roomId: response.roomId,
       name: response.name,
     };
   }
