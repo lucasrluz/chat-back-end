8cba1f557d7d0fa2de528f32b76531dc8432bb4b
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-02 13:50:06 -0300

    caso de uso para deletar sala

---
+++ src/use-case/room/delete-room-use-case.ts
@@ -0,0 +1,20 @@
+import { RoomRepositoryInterface } from '../../infra/repositories/room-repository-interface';
+import { error, success } from '../../shared/response';
+
+export class DeleteRoomUseCase {
+  private roomRepository: RoomRepositoryInterface;
+
+  constructor(roomRepository: RoomRepositoryInterface) {
+    this.roomRepository = roomRepository;
+  }
+
+  public async perform(roomId: string) {
+    const roomOrEmpty = this.roomRepository.findById(roomId);
+
+    if (!roomOrEmpty) return error('Room not found');
+
+    await this.roomRepository.deleteById(roomId);
+
+    return success();
+  }
+}
