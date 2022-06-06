2055b6bfcdaef5bca2479864b647fc875bb32d8d
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-06 09:06:15 -0300

    rota para deletar sala

--- src/infra/external/express/routes/room-route.ts
+++ src/infra/external/express/routes/room-route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { ensureAuthenticatedUserAdaptRoute } from './adapters/auth/adapt-route-ensure-authenticated-user';
 import { createRoomAdaptRoute } from './adapters/room/create-room-adapt-route';
+import { deleteRoomAdaptRoute } from './adapters/room/delete-room-adapt-route';
 import { editRoomAdaptRoute } from './adapters/room/edit-room-adapt-route';
 
 export const roomRouter = Router();
@@ -15,4 +16,10 @@ roomRouter.put(
   '/room/:room_id/:user_id',
   ensureAuthenticatedUserAdaptRoute(),
   editRoomAdaptRoute(),
+);
+
+roomRouter.delete(
+  '/room/:room_id/:user_id',
+  ensureAuthenticatedUserAdaptRoute(),
+  deleteRoomAdaptRoute(),
 );
