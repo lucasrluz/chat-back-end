547b7b40739ed4c8876f6a3b6102fdba1b8e68da
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-06 09:07:01 -0300

    testes na rota para deletar sala

---
+++ test/routes/room/delete-room.test.ts
@@ -0,0 +1,76 @@
+import request from 'supertest';
+import { createHashPassword } from '../../../src/infra/external/bcrypt/create-hash-password';
+import { app } from '../../../src/infra/external/express/app';
+import { UserTestsRepository } from '../../util/repository/user-tests-repository';
+
+describe('Tests on the delete room route ', () => {
+  const userTestRepository = new UserTestsRepository();
+
+  beforeAll(async () => {
+    await userTestRepository.deleteMany();
+  });
+
+  it('Should delete room', async () => {
+    const userData = {
+      username: 'a',
+      email: 'a@gmail.com',
+      password: await createHashPassword('123456'),
+    };
+
+    const loginData = {
+      username: 'a',
+      password: '123456',
+    };
+
+    const roomData = {
+      name: 'a',
+    };
+
+    const createUserResponse = await userTestRepository.create(userData);
+
+    const loginResponse = await request(app).post('/login').send(loginData);
+
+    const accessToken = loginResponse.body.accessToken;
+
+    const createRoomResponse = await request(app)
+      .post(`/room/${createUserResponse.userId}`)
+      .send(roomData)
+      .auth(accessToken, { type: 'bearer' });
+
+    const deleteRoomResponse = await request(app)
+      .delete(
+        `/room/${createRoomResponse.body.roomId}/${createUserResponse.userId}`,
+      )
+      .auth(accessToken, { type: 'bearer' });
+
+    expect(deleteRoomResponse.status).toEqual(200);
+
+    await userTestRepository.deleteMany();
+  });
+
+  it('Should return error message', async () => {
+    const userData = {
+      username: 'a',
+      email: 'a@gmail.com',
+      password: await createHashPassword('123456'),
+    };
+
+    const loginData = {
+      username: 'a',
+      password: '123456',
+    };
+
+    const createUserResponse = await userTestRepository.create(userData);
+
+    const loginResponse = await request(app).post('/login').send(loginData);
+
+    const accessToken = loginResponse.body.accessToken;
+
+    const deleteRoomResponse = await request(app)
+      .delete(`/room/'invalidRoomId'/${createUserResponse.userId}`)
+      .auth(accessToken, { type: 'bearer' });
+
+    expect(deleteRoomResponse.status).toEqual(404);
+    expect(deleteRoomResponse.body).toEqual('Room not found');
+  });
+});
