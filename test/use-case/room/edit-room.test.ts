e3ad1852868d1ffc8cbc9d73ba26b95664d4079c
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-08 14:52:43 -0300

    adição de teste para editar sala

--- test/use-case/room/edit-room.test.ts
+++ test/use-case/room/edit-room.test.ts
@@ -40,4 +40,24 @@ describe('Edit room use case tests', () => {
     expect(editRoomResponse.isError()).toEqual(true);
     expect(editRoomResponse.value).toEqual('Room not found');
   });
+
+  it('Should return error message', async () => {
+    const roomData = {
+      name: 'a',
+    };
+
+    const editRoomData = {
+      name: '',
+    };
+
+    const createRoomResponse = await createRoomUseCase.perform(roomData.name);
+
+    const editRoomResponse = await editRoomUseCase.perform(
+      createRoomResponse.value.roomId,
+      editRoomData.name,
+    );
+
+    expect(editRoomResponse.isError()).toEqual(true);
+    expect(editRoomResponse.value).toEqual('Name should not be empty');
+  });
 });
