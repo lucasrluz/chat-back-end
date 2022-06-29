7bd63deddead5bb1bd0842719c3b3ae95d6f647e
lucasrluz <lucasr.luzbr@gmail.com>
2022-06-29 08:52:55 -0300

    adição de novo teste no caso de uso para editar usuário

--- test/use-case/user/edit-user-use-case.test.ts
+++ test/use-case/user/edit-user-use-case.test.ts
@@ -1,11 +1,9 @@
-import { CreateUserUseCase } from '../../../src/use-case/user/create-user-use-case';
 import { EditUserUseCase } from '../../../src/use-case/user/edit-user-use-case';
 import { FakeUserRepository } from '../../util/fake-repository/fake-user-repository';
 
 describe('Edit user use case tests', () => {
   const userRepository = new FakeUserRepository();
 
-  const createUserUseCase = new CreateUserUseCase(userRepository);
   const editUserUseCase = new EditUserUseCase(userRepository);
 
   it('Should return user edited', async () => {
@@ -87,5 +85,29 @@ describe('Edit user use case tests', () => {
 
     expect(editUserResponse.isError()).toEqual(true);
     expect(editUserResponse.value).toEqual('Username should not be empty');
+  });
+
+  it('Should not edit user', async () => {
+    const editUserData = {
+      userId: 'userId',
+      username: 'username',
+      password: '',
+    };
+
+    jest.spyOn(userRepository, 'findById').mockReturnValue(
+      Promise.resolve({
+        id: 'userId',
+        username: 'username',
+        email: 'userEmail',
+        password: 'userPassword',
+      }),
+    );
+
+    const editUserResponse = await editUserUseCase.perform(editUserData);
+
+    expect(editUserResponse.isError()).toEqual(true);
+    expect(editUserResponse.value).toEqual(
+      'Password length must be longer than 6 characters',
+    );
   });
 });
